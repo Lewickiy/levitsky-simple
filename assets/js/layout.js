9da91c70412979c52530347eeab03c9718bdbaf3
Levitsky <lewickiy@gmail.com>
2026-01-16 23:51:36 +0300

    snippets added

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -2,7 +2,7 @@ async function loadTopMenu() {
     const container = document.getElementById("top-menu");
     if (!container) return;
 
-    const res = await fetch("partials/top-menu.html");
+    const res = await fetch("/partials/top-menu.html");
     container.innerHTML = await res.text();
 
     document.dispatchEvent(new Event("layout:ready"));
