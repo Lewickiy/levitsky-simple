2c1077a6a25772bfcc56e9f208791141f4b2db3e
Levitsky <lewickiy@gmail.com>
2026-01-16 23:55:04 +0300

    snippets added

--- assets/js/i18n.js
+++ assets/js/i18n.js
@@ -3,7 +3,7 @@ console.log("loaded: i18n.js");
 let i18nDict = {};
 
 async function loadI18n() {
-  const res = await fetch("data/i18n.json");
+  const res = await fetch("/data/i18n.json");
   if (!res.ok) {
     console.error("Failed to load i18n.json");
     return;
